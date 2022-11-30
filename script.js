2d49f6595c64dc87ec5be54cbe820619d077adbe
Tobias Wedhorn <tobias.wedhorn@gmx.de>
2022-11-30 22:12:12 +0100

    changed name of add function

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const inputField = document.querySelector('input');
 const button = document.querySelector('.add-button');
 const form = document.querySelector('form');
 
-function addFunction (e) {
-    // e.preventDefault();
+function addTask (e) {
+    e.preventDefault();
     console.log(inputField.value);
 }
 
-form.addEventListener('submit', addFunction)
+form.addEventListener('submit', addTask)
