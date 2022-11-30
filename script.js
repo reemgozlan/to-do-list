6e2d584cd8f3e53ca8fa44e857dc22568628a4ce
Tobias Wedhorn <tobias.wedhorn@gmx.de>
2022-11-30 16:58:59 +0100

    reading of input field and log to console added

---
+++ script.js
@@ -0,0 +1,10 @@
+const inputField = document.querySelector('input');
+const button = document.querySelector('.add-button');
+const form = document.querySelector('form');
+
+function addFunction (e) {
+    e.preventDefault();
+    console.log(inputField.value);
+}
+
+form.addEventListener('submit', addFunction)
