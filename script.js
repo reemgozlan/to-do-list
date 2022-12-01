2f4aa2b5b62902bb190711e9764036a47e651de6
Tobias Wedhorn <tobias.wedhorn@gmx.de>
2022-12-01 16:39:51 +0100

    finalized addTask

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-const inputField = document.querySelector('input');
-const button = document.querySelector('.add-button');
-const form = document.querySelector('form');
+const inputField = document.querySelector("input");
+const button = document.querySelector(".add-button");
+const form = document.querySelector("form");
+const ol = document.querySelector("ol");
 
-function addTask (e) {
-    e.preventDefault();
-    console.log(inputField.value);
+function addTask(e) {
+  e.preventDefault();
+  console.log(inputField.value);
+  ol.innerHTML += `
+    <li>
+    <div class="clear-element">
+        <input type="checkbox">
+        ${inputField.value} 
+        <div class="float-right">
+            <button type="button">Edit</button>
+            <button type="button">Delete</button>
+        </div>
+    
+    </div>
+    
+    </li>
+`;
 }
 
-form.addEventListener('submit', addTask)
+form.addEventListener("submit", addTask);
+
+
