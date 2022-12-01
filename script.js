4f4b649e09706073168b4c1b78c3c47bc98a8349
reemgozlan <118187653+reemgozlan@users.noreply.github.com>
2022-12-01 23:38:15 +0100

    Delete button has been modified

--- script.js
+++ script.js
@@ -2,18 +2,22 @@ const inputField = document.querySelector("input");
 const button = document.querySelector(".add-button");
 const form = document.querySelector("form");
 const ol = document.querySelector("ol");
+//const del_button = document.getElementById("del")
 
 function addTask(e) {
+    
   e.preventDefault();
+  console.log(e)
+  console.log(e.submitter)
   console.log(inputField.value);
   ol.innerHTML += `
     <li>
-    <div class="clear-element">
+    <div class="clear-element"">
         <input type="checkbox">
         ${inputField.value} 
         <div class="float-right">
             <button type="button">Edit</button>
-            <button type="button">Delete</button>
+            <button type="submit">Delete</button>
         </div>
     
     </div>
@@ -21,7 +25,14 @@ function addTask(e) {
     </li>
 `;
 }
+function delTask(id){
+console.log('hello')
+task  = document.getElementById(id)
+console.log(task)
+task.remove()
+}
 
 form.addEventListener("submit", addTask);
+// del_button.addEventListener("click",delTask)
 
 
