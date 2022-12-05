9cfee753cee3c9cff0e3b8f8144d78beb6243771
Tobias Wedhorn <tobias.wedhorn@gmx.de>
2022-12-05 14:25:40 +0100

    Co-authored-by: Sarah <sarahliess@users.noreply.github.com>

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ const inputField = document.querySelector("input");
 const button = document.querySelector(".add-button");
 const form = document.querySelector("form");
 const ol = document.querySelector("ol");
+const editBtns = document.querySelectorAll(".edit-button");
 
 function addTask(e) {
   e.preventDefault();
-  console.log(inputField.value);
   ol.innerHTML += `
     <li>
     <div class="clear-element">
         <input type="checkbox">
-        ${inputField.value} 
+        <input type="text" value="${inputField.value}" id="task-field" readonly >  
         <div class="float-right">
             <button type="button">Edit</button>
             <button type="button">Delete</button>
@@ -22,6 +22,27 @@ function addTask(e) {
 `;
 }
 
+function editTask(e) {
+  e.target
+    .closest(".clear-element")
+    .querySelector("#task-field").readOnly = false;
+  e.target.innerHTML = "Save";
+}
+
+function saveChanges(e) {
+  e.target
+    .closest(".clear-element")
+    .querySelector("#task-field").readOnly = true;
+  e.target.innerHTML = "Edit";
+}
+
 form.addEventListener("submit", addTask);
-
-
+editBtns.forEach((elem) => {
+  elem.addEventListener("click", (e) => {
+    if (e.target.innerHTML === "Save") {
+      saveChanges(e);
+    } else {
+      editTask(e);
+    }
+  });
+});
